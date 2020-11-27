5a43bc9d2f9d303df6fcc5e00d50dfaa14e767d2
fanjiang0619 <fanjiang0619@gmail.com>
2020-11-27 15:08:34 -0500

    2020.11.27

--- Array/Easy/Single Number.py
+++ Array/Easy/Single Number.py
@@ -1,7 +1,17 @@
 class Solution:
-    def singleNumber(self, nums):
+    def singleNumber1(self, nums):
         unique = list(set(nums))
         double = unique + unique
         for i in nums:
             double.remove(i)
         return int(double[0])
+
+    def singleNumber2(self, nums):
+        result = 2 * sum(set(nums)) - sum(nums)
+        return result
+
+    def singleNumber3(self, nums):
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
