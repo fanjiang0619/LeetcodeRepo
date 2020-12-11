41d7236fdf7b994a0251fb8c60b1bf3b55bcdec9
fanjiang0619 <fanjiang0619@gmail.com>
2020-12-11 16:55:15 -0500

    2020.12.11

---
+++ Strings/Easy/Reverse String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
