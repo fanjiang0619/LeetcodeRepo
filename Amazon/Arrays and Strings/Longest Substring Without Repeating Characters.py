c90005c12192ec6dc778a353424fc86aabc30b33
fanjiang0619 <fanjiang0619@gmail.com>
2020-12-18 00:09:10 -0500

    2020.12.17

--- Amazon/Arrays and Strings/Longest Substring Without Repeating Characters.py
+++ Amazon/Arrays and Strings/Longest Substring Without Repeating Characters.py
@@ -18,4 +18,20 @@ class Solution:
                 if count == maximum:
                     return count
         return max(count, longest)
-                            
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+        length, start = 0, 0
+        maximum = len(set(s))
+        visited = {}
+        for i, c in enumerate(s):
+            if c in visited and start <= visited[c]:
+                length = max(length, i - start)
+                start = visited[c] + 1
+            else:
+                if i - start == maximum:
+                    return maximum
+            visited[c] = i
+        return max(length, len(s) - start)
